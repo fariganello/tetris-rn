4d6c507474b8a32f6cff69ab87c9796068ed83d9
fariganello <fariganello@gmail.com>
2021-02-20 04:07:13 -0300

    created Level component and linked to lines cleared. Increased speed with level up

---
+++ src/components/Level.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const Level = ({level}) => {
+    return (
+        <View style={styles.linesCounter}>
+            <Text style={styles.title}>LEVEL</Text>
+            <Text>{level}</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+  linesCounter: {
+    position: "absolute",
+    right: 0,
+    bottom: 50,
+    height: 50,
+    width: 80,
+    alignSelf: 'flex-end',
+    flexDirection: "column",
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: "bold",
+  }
+});
+
+export default Level;
